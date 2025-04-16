567f33ea2a924af3404ca07d043f94e7d5ea2efa
Olena.Holub <oleholu@gmail.com>
2025-04-16 11:55:09 +0200

    add translate to google login

--- src/pages/GoogleCallback.jsx
+++ src/pages/GoogleCallback.jsx
@@ -3,10 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import Swal from "sweetalert2";
 import { loginWithGoogle } from "../api/auth";
+import { useTranslation } from "react-i18next";
 
 export default function GoogleCallback() {
   const { setUser } = useAuth();
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   useEffect(() => {
     const fetchGoogleUser = async () => {
@@ -14,7 +16,7 @@ export default function GoogleCallback() {
       const code = urlParams.get("code");
 
       if (!code) {
-        Swal.fire("Error", "No authorization code received from Google.", "error");
+        Swal.fire(t("error"), t("login.google_login_error"), "error");
         return navigate("/login");
       }
 
@@ -24,18 +26,18 @@ export default function GoogleCallback() {
           setUser(user);
           navigate("/dashboard");
         } else {
-          Swal.fire("Error", "Google login failed to return user.", "error");
+          Swal.fire(t("error"), t("login.google_login_error"), "error");
           navigate("/login");
         }
       } catch (err) {
         console.error("Google login failed:", err);
-        Swal.fire("Error", "Something went wrong during Google login.", "error");
+        Swal.fire(t("error"), t("login.google_login_error"), "error");
         navigate("/login");
       }
     };
 
     fetchGoogleUser();
-  }, [navigate, setUser]);
+  }, [navigate, setUser, t]);
 
-  return <p className="text-center mt-8 text-white">Loading...</p>;
+  return <p className="text-center mt-8 text-white">{t("loading")}</p>;
 }
