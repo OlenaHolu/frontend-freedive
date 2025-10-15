4a388637811d173403aa0cbc483e57ef62fccbdc
Olena.Holub <oleholu@gmail.com>
2025-10-15 14:50:18 +0200

    feat: add initial FriendsPage component

---
+++ src/pages/FriendsPage/index.jsx
@@ -0,0 +1,8 @@
+export default function FriendsPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Friends Page</h1>
+      <p>This is the Friends Page. Content coming soon!</p>
+    </div>
+  );
+}
